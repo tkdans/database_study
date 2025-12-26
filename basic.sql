a6312bc55448bd2248c7bc35237d9ce59e5389a8
tkdans <psmoon81@naver.com>
2025-12-26 11:40:43 +0900

    DB 시작

---
+++ basic.sql
@@ -0,0 +1,25 @@
+--SQL QUARYY 주석
+-- 한글 주석
+
+DB(DataBase)데이터가 저장되는 공간
+DBMS (DataBase Management System) 데이터베이스 관리 시스템
+
+Windows OS->Oracle DBMS <- 접속 틀(sqldeveloper...)로 사용자가 접속 후 사용
+                            (SQL 쿼리로 DB데이터를 사용/조작)
+*접속계정
+id : scott
+pw : tiger
+
+SQL (쿼리문)
+Structured Query Language
+
+
+DB 사용/조작 -> 데이터 조작 명령(퀴리문을 작성해서)
+CRUD
+Create 생성
+Read 조회(읽기)
+Update 수정
+Delete 삭제
+
+
+
